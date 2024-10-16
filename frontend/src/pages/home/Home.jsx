aa01d4265e4f3201b7c51fd53ccc8601d59fa82c
LanreOlaitanJr <olaitanjr25@gmail.com>
2024-10-16 18:35:36 +0100

    4th part completed - socket

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Sidebar from "../../components/sidebar/Sidebar";
 const Home = () => {
 	return (
 		<div className='flex sm:h-[450px] md:h-[550px] rounded-lg overflow-hidden bg-gray-400 bg-clip-padding backdrop-filter backdrop-blur-lg bg-opacity-0'>
+			<img src="/frontend/public/chat-app-background.jpg" alt="" />
 			<Sidebar />
 			<MessageContainer />
 		</div>
